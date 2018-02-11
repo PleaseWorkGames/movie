bbd34a6e9f6d8f2cb4ca88773b4c8f1e8f7dc969
Dan Mooney <dan.r.mooney@gmail.com>
2018-02-11 16:12:02 -0600

    Add/calculate accuracy float

--- Assets/Scripts/Mouse.cs
+++ Assets/Scripts/Mouse.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,11 @@ public class Mouse : MonoBehaviour
     
     private Vector3 lastMouseCoordinate = Vector3.zero;
 
+    private float startingYPosition;
+    
     private string currentDirection;
+
+    private float accuracy = 0;
     
     // Use this for initialization
     void Start()
@@ -26,23 +31,43 @@ public class Mouse : MonoBehaviour
         if (mouseDelta.x < 0 && currentDirection != "left") { // Assuming a negative value is to the left.
             Debug.Log("Going to left");
             currentDirection = "left";
-            resetYPositions();
+//            resetYPositions();
+            storeStartingYPosition();
         } else if (mouseDelta.x > 0 && currentDirection != "right") {
             Debug.Log("Going to right");
             currentDirection = "right";
-            resetYPositions();
+//            resetYPositions();
+            storeStartingYPosition();
         } else {
-            Debug.Log(mouseDelta);
+//            Debug.Log(mouseDelta);
+            logAccuracy(mouseDelta);
         }
-        
         
         
         // Then we store our mousePosition so that we can check it again next frame.
         lastMouseCoordinate = Input.mousePosition;
         
-        yPositions.Add(lastMouseCoordinate.y);
+//        yPositions.Add(lastMouseCoordinate.y);
     }
 
+    private void logAccuracy(Vector3 mouseDelta)
+    {
+        var mouseDeltaX = Math.Abs(mouseDelta.x);
+        var mouseDeltaY = Math.Abs(mouseDelta.y);
+        
+        if (mouseDeltaY >= 1) {
+            return;
+        }
+        
+        accuracy += mouseDeltaX * (1 - mouseDeltaY);
+        Debug.Log(accuracy);
+    }
+    
+    private void storeStartingYPosition()
+    {
+        startingYPosition = Input.mousePosition.y;
+    }
+    
     private void resetYPositions()
     {
         yPositions.Clear();
